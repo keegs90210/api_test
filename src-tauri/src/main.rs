abcb22cd41ccc167ca70f88cd3770896bbac8c73
keegs90210 <105447591+keegs90210@users.noreply.github.com>
2025-02-07 10:45:53 +0200

    working request + embedded

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,4 +1,7 @@
+
 use api_test_lib::run;  // Correctly import from the library
+
+
 fn main() {
     // Call the run function from lib.rs to start the application
     api_test_lib::run();  // Assuming your package name is `api_test`
